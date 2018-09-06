40a660c52594624c0ad7ad819a5e2e2c3ab9197a
Toru Nayuki <tnayuki@icloud.com>
2018-09-06 22:11:04 +0900

    PDCA mode

---
+++ Assets/Scripts/PDCA.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PDCA : MonoBehaviour {
+	private bool showPDCA;
+
+	void OnGUI() {
+		showPDCA = GUI.Toggle(new Rect(10, 10, 200, 30), showPDCA, "PDCA");
+
+		transform.localScale = (showPDCA) ? new Vector3(1.0f, 1.0f, 1.0f) : new Vector3(0.0f, 0.0f, 0.0f);
+	}
+}
